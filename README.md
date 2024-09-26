3706f4a8c4e2e4a20c09a8feb3f041a2a731d0a9
John Poth <poth.john@gmail.com>
2024-09-26 14:40:05 +0200

    Use Jaeger UI and log exporter

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # Camel CXF OpenTelemetry reproducer
 
-## OpenTelemetry agent
-
-Please download the OpenTelemetry agent from https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.8.0/opentelemetry-javaagent.jar and save it as `etc/opentelemetry-javaagent-2.8.0.jar`.
-
 ## Running
 
-- Start the `reproducer` project
-- If you're using IntelliJ IDEA, there's a sample request file available at `reproducer/src/test/resources/test-requests.http`.
 
-### Traces without fixes
-
-- CXF async mode: [<img src="images/cxf-async-no-fixes.png">]
-- CXF sync mode: [<img src="images/cxf-sync-no-fixes.png">]
+- Run Jaeger UI, in reproducer folder : `docker compose up -d`
+- Start the `reproducer`, in reproducer folder : mvn clean spring-boot:run
+- Send request in root folder: `curl -i localhost:8899/services/otel-main-service -X POST --header 'Content-Type: application/xml;charset=UTF-8' -d @./reproducer/src/main/resources/requests/sample-request.xml`
+- View Traces in Jaeger http://localhost:16686/ also in logs (search for `OtlpJsonLoggingSpanExporter`)
--- reproducer/src/main/java/my/example/otel/reproducer/support/CxfBeans.java
+++ reproducer/src/main/java/my/example/otel/reproducer/support/CxfBeans.java
@@ -1,11 +1,20 @@
 package my.example.otel.reproducer.support;
 
+import java.util.List;
+
 import https.www_w3schools_com.xml.TempConvertSoap;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.logging.otlp.OtlpJsonLoggingSpanExporter;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
 import jakarta.annotation.PostConstruct;
 import org.apache.camel.component.cxf.common.DataFormat;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
@@ -15,8 +24,13 @@ import org.apache.camel.pizza.Pizza;
 import org.apache.cxf.tracing.opentelemetry.OpenTelemetryClientFeature;
 import org.apache.cxf.tracing.opentelemetry.OpenTelemetryFeature;
 import org.apache.cxf.tracing.opentelemetry.jaxrs.OpenTelemetryProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.tracing.SpanProcessors;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -30,6 +44,16 @@ class CxfBeans {
         OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder()
                 .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance())).build();
         GlobalOpenTelemetry.set(openTelemetry);
+    }
+
+    @Bean
+    public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+        return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
+    }
+
+    @Bean
+    public SpanExporter otlpJsonSpanExporter() {
+        return OtlpJsonLoggingSpanExporter.create();
     }
 
     @Bean
